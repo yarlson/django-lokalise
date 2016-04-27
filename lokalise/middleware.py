2a2d68a3abe60a6901a141f0a1e58bb8671a1273
Yar Kravtsov <yarlson@gmail.com>
2016-04-27 07:49:28 +0300

    reload locale by webhook

--- lokalise/middleware.py
+++ lokalise/middleware.py
@@ -1,7 +1,19 @@
+import threading
+from os.path import getmtime
+
 from django.utils.autoreload import reset_translations
+
+from . import get_locale_path
 
 
 class ReloadTranslationsMiddleware(object):
     @staticmethod
     def process_request(request):
-        reset_translations()
+        locale_mtime = getmtime(get_locale_path())
+        try:
+            thread_local = threading.local
+            if locale_mtime > thread_local.locale_mtime:
+                thread_local.locale_mtime = locale_mtime
+                reset_translations()
+        except:
+            pass
